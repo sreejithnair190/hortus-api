88ba01a54361322a0a85ec066cd665ac7547d3a7
Sreejith Nair <sreejith190nair@gmail.com>
2023-05-01 22:04:47 +0530

    changed port and added new package

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const plantRouter = require('./routes/plantRoutes')
 
@@ -7,6 +8,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 
 // if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
