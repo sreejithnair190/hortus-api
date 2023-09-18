10120d4842091bdab3f136ed5144c15e157d7ddf
sreejithnair190 <sreejith190nair@gmail.com>
2023-09-18 23:34:17 +0530

    email bug fix

--- services/emailService.js
+++ services/emailService.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 const pug = require("pug");
 const htmlToText = require("html-to-text");
 
-module.exports = class Email {
+class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(" ")[0];
@@ -11,7 +11,6 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === "production") {
       return nodemailer.createTransport({
         host: process.env.BREVO_HOST,
         port: 587,
@@ -23,21 +22,11 @@ module.exports = class Email {
         //   rejectUnauthorized: false, // Accept self-signed certificates - Uncomment if error arises
         // },
       });
-    }
-
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
   }
 
   async send(template, subject) {
     const html = pug.renderFile(
-      `${__dirname}/../public/emailTemplates/${template}.pug`,
+      `${__dirname}/../public/mail/${template}.pug`,
       {
         firstName: this.firstName,
         url: this.url,
@@ -63,4 +52,6 @@ module.exports = class Email {
   async sendPasswordReset() {
     await this.send("passwordReset", "Password Reset");
   }
-};
+}
+
+module.exports = Email;
